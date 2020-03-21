adb761ae0431b92c332118e479c24f2077a3aa04
jeffreyadamo <58490053+jeffreyadamo@users.noreply.github.com>
2020-03-20 23:00:06 -0700

    Mostly complete. Bug in output length is limited to characterType length

--- README.md
+++ README.md
@@ -4,16 +4,21 @@ GIVEN I need a new, secure password.
 
 WHEN I click the button to generate a password, THEN I am presented with a series of prompts for password criteria:
 
-	1) WHEN prompted for password criteria THEN I select which criteria to include in the password 
+	1) WHEN prompted for password criteria THEN I select which criteria to include in the password
+        Done!
 		
 	2) WHEN prompted for the length of the password THEN I choose a length of at least 8 characters and no more than 128 characters
-		a. Done
+		Done!
 		
-	3) WHEN prompted for character types to include in the password THEN I choose lowercase, uppercase, numeric, and/or special characters 
+	3) WHEN prompted for character types to include in the password THEN I choose lowercase, uppercase, numeric, and/or special characters
+        Done! 
 		
 	4) WHEN I answer each prompt THEN my input should be validated and at least one character type should be selected 
+        Done!
 		
 	5) WHEN all prompts are answered THEN a password is generated that matches the selected criteria
-		a.  
+		Close. Cannot create passwords in length beyond the number of characters available in characterTypes array.
+
 	6) WHEN the password is generated THEN the password is either displayed in an alert or written to the page
+        Done!
 		
--- script.js
+++ script.js
@@ -4,13 +4,30 @@ var lowercase;
 var uppercase;
 var numeric;
 var specialCharacters;
+var characterTypes = [];
 var password1 = [];
+var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var special = "~!@#$%^&*?";
+var selectedCharacter = [];
 
 function generatePassword() {
   /****
    * WRITE YOUR CODE HERE
    */
-
+  var password1 = [];
+  var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "0123456789";
+  var special = "~!@#$%^&*?";
+  selectedCharacter = [];
+  characterTypes = [];
+  lowercase = false;
+  uppercase = false;
+  numeric = false;
+  specialCharacters = false;
+  
   // 
   // 1) WHEN prompted for password criteria THEN I select which criteria to include in the password 
 
@@ -18,133 +35,121 @@ function generatePassword() {
 
   //   //    Define password length. Once "Generate Password" is clicked, it will run the generatePassword function. We are to prompt the user with how long they want the password to be. It must be at least 8 characters long and less than 128 characters. User can cancel out of the function. Password length is then verified to the user.
 
-
-
   var passwordLength = prompt("How long would you like your password to be? (min 8; max 128)");
-  if (passwordLength === null) {
-    return;
-  }
-  if (passwordLength < 8 || passwordLength > 128) {
-    var passwordLength = alert("Improper password length.\nPassword must be between 8 and 128 characters.");
-    generatePassword();
-    return;
-  }
+    if (passwordLength === null) {
+      return;
+    }
 
-  else {
-    alert("You've chosen a password that is " + passwordLength + " characters long\nYou may chose between the following chracter types:\nlowercase\nuppercase\nnumeric\nspecial characters");
-  }
-  console.log(passwordLength);
+    if (passwordLength < 8 || passwordLength > 128) {
+      var passwordLength = alert("Improper password length!\n\nPassword must be between 8 and 128 characters");
+      generatePassword();
+      return;
+    }
+
+    // else {
+    //   alert("You've chosen a password that is " + passwordLength + " characters long\nYou may chose between the following character types:\nLowercase Letters\nUppercase Letters\nNumbers\nSpecial Characters");
+    // }
+    else {
+      var confirmLength = confirm("You've chosen a password that is " + passwordLength + " characters long\n\nYou may chose between the following character types:\n   Lowercase Letters\n   Uppercase Letters\n   Numbers\n   Special Characters\n\n(Press 'OK to continue or 'cancel' for a different password length)");
+      if (confirmLength === false) {
+        generatePassword();
+      }
+    }
+
+  console.log("User input password length: " + passwordLength);
+  console.log("-----------");
+  
+  // 	3) WHEN prompted for character types to include in the password THEN I choose lowercase, uppercase, numeric, and/or special characters 
+  // // I can do this with confirms. I'd like to be able to do this all at once though://
+
+  function characters() {
+    selectedCharacter = [];
+    lowercase = confirm("Would you like to include Lowercase Letters?\n\n(Press 'OK for yes and 'cancel' for no)");
+      
+      if (lowercase) {
+        for (i = 0; i < lowercaseLetters.length; i++) {
+          selectedCharacter.push(lowercaseLetters[i]);
+        }
+        console.log("lowercase letters have been chosen");
+        characterTypes.push("Lowercase Letters");
+      }
+
+    uppercase = confirm("Would you like to include Uppercase Letters?\n\n(Press 'OK for yes and 'cancel' for no)");
+      
+      if (uppercase) {
+        for (i = 0; i < uppercaseLetters.length; i++) {
+          selectedCharacter.push(uppercaseLetters[i]);
+          
+        }
+        console.log("uppercase letters have been chosen");
+        characterTypes.push("Uppercase Letters");
+      }
+
+
+    numeric = confirm("Would you like to include Numbers?\n\n(Press 'OK for yes and 'cancel' for no)");
+      if (numeric) {
+        for (i = 0; i < numbers.length; i++) {
+          selectedCharacter.push(numbers[i]);
+        }
+        console.log("numbers have been chosen");
+        characterTypes.push("Numbers");
+      }
+
+
+    specialCharacters = confirm("Would you like to include Special Characters?\n\n(Press 'OK for yes and 'cancel' for no)");
+      if (specialCharacters) {
+        for (i = 0; i < special.length; i++) {
+          selectedCharacter.push(special[i]);
+        }
+        console.log("special characters have been chosen");
+        characterTypes.push("Special Characters");
+      }
+
+    console.log("-----------");  
+    console.log("characterTypes = " + characterTypes);  
+
+    if (lowercase === false && uppercase === false && numeric === false && specialCharacters === false) {
+      alert("At least one character type must be selected!!\n\nPress OK to try again");
+      characters();
+    }
+
+    console.log("-----------");
+
+    var continue2 = confirm("You have selected the following character types:\n\n" + characterTypes.join("\n") + "\n\nPress OK to continue or cancel to reset choices");
+
+    if (continue2 === false){
+      characterTypes = [];
+      console.log("Resetting character types");
+      console.log(characterTypes);
+      characters();
+      }
+
+
+  } //end of function characters
+
+  //Execute the function characters
   characters();
-// }
 
-// 	3) WHEN prompted for character types to include in the password THEN I choose lowercase, uppercase, numeric, and/or special characters 
-
-// I can do this with confirms. I'd like to be able to do this all at once though://
-function characters() {
-  // alert("You may chose between lowercase, uppercase, numeric, and/or special characters");
-  lowercase = confirm("would you like to include lowercase letters?");
-  uppercase = confirm("would you like to include uppercase letters?");
-  numeric = confirm("would you like to include numbers?");
-  specialCharacters = confirm("would you like to include special characters?");
-  characterTypes = [lowercase, uppercase, numeric, specialCharacters];
-
-  console.log(lowercase);
-  console.log(uppercase);
-  console.log(numeric);
-  console.log(specialCharacters);
-  console.log(characterTypes);
-
-  if (lowercase === false && uppercase === false && numeric === false && specialCharacters === false) {
-    alert("at least one characterType must be selected");
-    characters();
-
-  }
-}
+  console.log("list of characters " + selectedCharacter);
+  console.log("passwordLength is " + passwordLength);
 
 
-// console.log("This is outside the function:" + lowercase);
-
-
-
-
-// Need an alert telling which characterTypes have been chosen
-
-// if (characterTypes) = [true, true, true, true]
-//   // [lowercase, uppercase, numeric, specialCharacters]
-// if (characterTypes) = [true, true, true, false]
-//   // [lowercase, uppercase, numeric, null]
-// if (characterTypes) = [true, true, false, false]
-//   // [lowercase, uppercase, null, null]
-// if (characterTypes) = [true, false, false, false]
-//   // [lowercase, null, null, null]
-// if (characterTypes) = [true, false, true, false]
-//   // [lowercase, uppercase, numeric, specialCharacters]
-
-// alert("you have chosen to include " + "insert characterTypes here");
-
-
-
-
-// Let's get random numbers
-var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
-var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numbers = "0123456789";
-var special = "~!@#$%^&*?";
-
-var selectedCharacter = [];
-
-if (lowercase) {
-  for (i = 0; i < lowercaseLetters.length; i++) {
-    selectedCharacter.push(lowercaseLetters[i]);
-  }
-}
-
-if (uppercase) {
-  for (i = 0; i < uppercaseLetters.length; i++) {
-    selectedCharacter.push(uppercaseLetters[i]);
-  }
-}
-
-if (numeric) {
-  for (i = 0; i < numbers.length; i++) {
-    selectedCharacter.push(numbers[i]);
-  }
-}
-
-if (specialCharacters) {
-  for (i = 0; i < special.length; i++) {
-    selectedCharacter.push(special[i]);
-  }
-}
-
-console.log("list of characters" + selectedCharacter);
-
-
-
-
-
-// This is how to generate a random number
-// var num = Math.floor(Math.random() * 10);
-// console.log(num);
-// console.log(special);
-
-
-
-// trying to get the loop to generate a defined amount of characters based on passwordLength, but it does not work, says: passwordLength is not defined
-
-// function randomNumbers() {
-
+  // This is how to generate a random number
+  // var num = Math.floor(Math.random() * 10);
+  // console.log(num);
+  // console.log(special);
 
 
   for (var i = 0; i < passwordLength; i++) {
-    password1.push(selectedCharacter[Math.floor(Math.random() * passwordLength)]);
+    password1.push(selectedCharacter[Math.floor(Math.random() * parseInt(passwordLength))]);
 
 
     // var num = Math.floor(Math.random() * 10);
     // console.log(num);
   }
 
-console.log(password1.join(""));
+  console.log("password1 is: " + password1.join(""));
+  console.log(password1.length);
 
 
   // Conditions:
@@ -153,38 +158,8 @@ console.log(password1.join(""));
   // }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   return password1.join("");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 //////////////////////////////////////////////////////////////
 // DO NOT TOUCH THE CODE BELOW
@@ -202,3 +177,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
